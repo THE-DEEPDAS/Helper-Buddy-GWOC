0bd211ff86adbb1f1451462c466e1f44ebc8da82
THE-DEEPDAS <deepdblm@gmail.com>
2025-07-19 18:31:46 +0530

    Final

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -88,7 +88,6 @@ export default function ServicesPage() {
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {services.map((service) => (
-              
               <ServiceCard key={service.id} service={service} />
             ))}
           </div>
